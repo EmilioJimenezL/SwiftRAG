cdfc47599a601a7cffa2b1c14d88f01d62b27fb8
Rafael <rafael05mej@gmail.com>
2026-05-22 09:02:20 -0600

    Initial Commit

---
+++ RAGTaller/RAGTallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAGTallerApp.swift
+//  RAGTaller
+//
+//  Created by Rafael on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RAGTallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
